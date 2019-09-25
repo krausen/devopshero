4c52ef5853c8743e85f2844ba031a0f6447f23eb
Niclas Krause <niclas@Niclass-MacBook-Air.local>
2019-09-25 13:00:04 -0700

    Add unit test for try to stop game

--- test/test_game.py
+++ test/test_game.py
@@ -1,28 +1,38 @@
-from unittest import mock, TestCase
+from unittest import mock
 import datetime
 
-from src.game import _sort_high_score, try_to_start_game
+from src.game import _sort_high_score, try_to_start_game, try_to_stop_game
 from src.models import Channel
 
 
-class TestGame(TestCase):
+@mock.patch('src.game.db')
+@mock.patch.object(Channel, 'query', auto_spec=True)
+def test_try_to_start_game(mock_channel_query, mock_db):
+  channel_mock = mock.Mock()
+  channel_mock.start = None
+  mock_channel_query.filter_by.return_value.first.return_value = channel_mock
 
-  @mock.patch('src.game.db')
-  @mock.patch.object(Channel, 'query', auto_spec=True)
-  def test_try_to_start_game(self, mock_channel_query, mock_db):
-    channel_mock = mock.Mock()
-    channel_mock.start = None
-    mock_channel_query.filter_by.return_value.first.return_value = channel_mock
-
-    try_to_start_game('channel_id')
-    
-    assert channel_mock.start
-    assert mock_db.session.commit.called_once()
+  try_to_start_game('channel_id')
+  
+  assert channel_mock.start
+  mock_db.session.commit.assert_called_once()
 
 
-  def test_sort_high_score(self):
-    high_score = {'user_a': 3, 'user_b': 4}
+@mock.patch('src.game._finish_game')
+@mock.patch.object(Channel, 'query', auto_spec=True)
+def test_try_to_stop_game(mock_channel_query, mock_finish_game):
+  channel_mock = mock.Mock()
+  channel_mock.start = datetime.datetime.now()
+  mock_channel_query.filter_by.return_value.first.return_value = channel_mock
 
-    sorted_high_score = _sort_high_score(high_score)
+  try_to_stop_game('channel_id')
 
-    assert high_score == {'user_b': 4, 'user_a': 3}
+  mock_finish_game.assert_called_once_with(channel_mock)
+
+
+def test_sort_high_score():
+  high_score = {'user_a': 3, 'user_b': 4}
+
+  sorted_high_score = _sort_high_score(high_score)
+
+  assert high_score == {'user_b': 4, 'user_a': 3}
